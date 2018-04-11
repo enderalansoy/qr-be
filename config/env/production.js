10a5d53f8e1ed9ad9fd6186836f6edb0cdcfffd1
eren-aydemir <eren.aydemir@gmail.com>
2018-04-11 16:05:00 +0300

    df

--- config/env/production.js
+++ config/env/production.js
@@ -254,10 +254,9 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://warm-crag-35465.herokuapp.com',
+    ],
 
 
     /***************************************************************************
